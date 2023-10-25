9bfab14d231bff8ca5cb1e74c2f8c73915fd42a4
Tiebe Groosman <tiebe.groosman@gmail.com>
2023-10-25 22:43:34 +0200

    Refactor account registration and update database setup
    Refactored the account registration process by removing the Register.kt and replacing it with AccountRouting.kt and RegisterRequest.kt. This change gives a clearer separation of responsibilities in the code. Adjusted the database setup methods in UsersSchema.kt, BearerSchema.kt and MagisterData.kt to create only missing tables and columns which offers better database management.

--- src/main/kotlin/dev/avt/api/accounts/AccountRouting.kt
+++ src/main/kotlin/dev/avt/api/accounts/AccountRouting.kt
@@ -1,6 +1,7 @@
 package dev.avt.api.accounts
 
 import dev.avt.api.accounts.login.loginRoutes
+import dev.avt.api.accounts.register.registerRoutes
 import io.ktor.server.routing.*
 
 fun Routing.accountRouting() {
---
+++ src/main/kotlin/dev/avt/api/accounts/register/Register.kt
@@ -0,0 +1,40 @@
+package dev.avt.api.accounts.register
+
+import dev.avt.api.accounts.login.LoginRequest
+import dev.avt.api.accounts.login.LoginResponse
+import dev.avt.database.AVTUser
+import dev.avt.database.UserService
+import dev.avt.database.UserService.Users.username
+import dev.avt.database.createBearerToken
+import dev.tiebe.magisterapi.api.account.LoginFlow
+import dev.tiebe.magisterapi.api.account.ProfileInfoFlow
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.and
+import org.jetbrains.exposed.sql.transactions.transaction
+
+fun Routing.registerRoutes() {
+    post("/api/accounts/register") {
+        val body = call.receive<RegisterRequest>()
+
+        val user = transaction {
+            AVTUser.new {
+                this.userName = body.username
+                this.password = body.password
+                this.email = body.email
+                this.firstName = body.firstName
+                this.lastName = body.lastName
+            }
+        }
+
+        val bearer = transaction {
+            user.createBearerToken()
+        }
+
+        call.respond(HttpStatusCode.OK, LoginResponse(user.id.value, bearer))
+    }
+}

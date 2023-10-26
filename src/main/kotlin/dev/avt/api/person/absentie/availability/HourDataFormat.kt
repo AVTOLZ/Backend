b9e720494bf18a9809f30761289822458129c49f
gulp1n <abeldenouden@hotmail.com>
2023-10-26 16:13:18 +0200

    did a todo

--- src/main/kotlin/dev/avt/api/person/absentie/availability/HourDataFormat.kt
+++ src/main/kotlin/dev/avt/api/person/absentie/availability/HourDataFormat.kt
@@ -3,4 +3,4 @@ package dev.avt.api.person.absentie.availability
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class HourDataFormat(val id: Int, val startTime: Long, val endTime: Long)
+data class HourDataFormat(val id: Int, val startTime: Long, val endTime: Long, val alreadyRegistered: Boolean)
--- src/main/kotlin/dev/avt/api/person/absentie/availability/ReadAvailability.kt
+++ src/main/kotlin/dev/avt/api/person/absentie/availability/ReadAvailability.kt
@@ -3,6 +3,9 @@ package dev.avt.api.person.absentie.availability
 import dev.avt.database.AVTUser
 import dev.avt.database.AvailableHoursService.AvailableHours.endTime
 import dev.avt.database.AvailableHoursTable
+import dev.avt.database.RegisteredHoursService.RegisteredHours.hour
+import dev.avt.database.RegisteredHoursService.RegisteredHours.user
+import dev.avt.database.RegisteredHoursTable
 import dev.avt.database.ge
 import io.ktor.http.*
 import io.ktor.server.application.*
@@ -10,6 +13,7 @@ import io.ktor.server.auth.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import kotlinx.datetime.Clock
+import org.jetbrains.exposed.sql.and
 import org.jetbrains.exposed.sql.transactions.transaction
 
 fun Routing.readAvailabilityRoutes(){
@@ -41,14 +45,23 @@ fun Routing.readAvailabilityRoutes(){
                     return@get
                 }
 
-                // TODO add system to show user if they already are registered for an hour
                 // also one of the main dissadvatages of an ide is that rn in vibing on the couch with my dog and my laptop on my lap, and dayum this thing is HOT
                 // also also my dog took the spot i was sitting
                 val allowedHours: Array<HourDataFormat> = emptyArray()
 
                 remainingHours.forEach {
                     if (reqUser.rank.ge(it.requiredRank)) {
-                        allowedHours + HourDataFormat(it.id.value, it.startTime, it.endTime)
+                        val alreadyRegisteredHour = transaction {
+                            RegisteredHoursTable.find { (user eq reqUser.id.value) and (hour eq it.id.value) }.firstOrNull()
+                        }
+
+                        var alreadyRegistered: Boolean = false
+
+                        if (alreadyRegisteredHour != null) {
+                            alreadyRegistered = true
+                        }
+
+                        allowedHours + HourDataFormat(it.id.value, it.startTime, it.endTime, alreadyRegistered)
                     }
                 }
 

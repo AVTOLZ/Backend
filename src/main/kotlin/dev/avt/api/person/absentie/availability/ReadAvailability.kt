250dfbb71214617e6c520dd0f7e6490ef11b7e83
gulp1n <abeldenouden@hotmail.com>
2023-10-26 14:55:10 +0200

    added some comments

--- src/main/kotlin/dev/avt/api/person/absentie/availability/ReadAvailability.kt
+++ src/main/kotlin/dev/avt/api/person/absentie/availability/ReadAvailability.kt
@@ -42,7 +42,7 @@ fun Routing.readAvailabilityRoutes(){
                     return@get
                 }
 
-                //TODO come up with a better system to send this data to the user
+                // TODO come up with a better system to send this data to the user
                 // also one of the main dissadvatages of an ide is that rn in vibing on the couch with my dog and my laptop on my lap, and dayum this thing is HOT
                 // also also my dog took the spot i was sitting
                 val allowedHours: Array<String> = emptyArray()

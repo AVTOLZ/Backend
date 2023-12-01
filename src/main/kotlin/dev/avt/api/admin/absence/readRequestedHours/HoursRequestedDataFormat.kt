9a536df652ce9e007e5206a5d4adabf19cb156ab
gulp1n <abeldenouden@hotmail.com>
2023-12-01 13:38:08 +0100

    changed name timeRequested to requestTime

--- src/main/kotlin/dev/avt/api/admin/absence/readRequestedHours/HoursRequestedDataFormat.kt
+++ src/main/kotlin/dev/avt/api/admin/absence/readRequestedHours/HoursRequestedDataFormat.kt
@@ -4,4 +4,4 @@ import kotlinx.serialization.Serializable
 
 // TODO add magister grade and test data
 @Serializable
-data class HoursRequestedDataFormat(val userId: Int, val startTime: Long, val endTime: Long, val timeRequested: Long, val id: Int)
+data class HoursRequestedDataFormat(val userId: Int, val startTime: Long, val endTime: Long, val requestTime: Long, val id: Int)

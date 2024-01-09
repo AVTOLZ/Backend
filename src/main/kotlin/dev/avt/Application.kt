60e8e4479c497520873dff2c2cabfbced55bfb39
Tiebe Groosman <tiebe.groosman@gmail.com>
2024-01-10 00:58:20 +0100

    mroe fixes

--- src/main/kotlin/dev/avt/Application.kt
+++ src/main/kotlin/dev/avt/Application.kt
@@ -2,9 +2,11 @@ package dev.avt
 
 import dev.avt.plugins.*
 import io.github.cdimascio.dotenv.dotenv
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.server.plugins.cors.routing.*
 
 val dotEnv = dotenv()
 
@@ -21,4 +23,13 @@ fun Application.module() {
     configureDatabases()
     configureSockets()
     configureRouting()
+
+    install(CORS) {
+        anyHost()
+
+        allowMethod(HttpMethod.Options)
+        allowMethod(HttpMethod.Put)
+        allowMethod(HttpMethod.Patch)
+        allowMethod(HttpMethod.Delete)
+    }
 }

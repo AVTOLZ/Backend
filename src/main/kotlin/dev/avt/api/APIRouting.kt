9669672e9b58918dc04ef0204e4f2cfd1b62081c
Tiebe Groosman <tiebe.groosman@gmail.com>
2024-01-10 18:24:28 +0100

    adminrouting

--- src/main/kotlin/dev/avt/api/APIRouting.kt
+++ src/main/kotlin/dev/avt/api/APIRouting.kt
@@ -1,6 +1,7 @@
 package dev.avt.api
 
 import dev.avt.api.accounts.accountRouting
+import dev.avt.api.admin.adminRouting
 import dev.avt.api.person.magister.magisterRouting
 import dev.avt.api.person.permissions.permissionRouting
 import dev.avt.api.person.personRouting
@@ -11,4 +12,5 @@ fun Routing.apiRouting() {
     magisterRouting()
     permissionRouting()
     personRouting()
+    adminRouting()
 }
